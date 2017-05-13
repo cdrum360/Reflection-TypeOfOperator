b40057f418995efdbc5487a8b38969f8e55a4646
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 13:35:37 -0400

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    public class BaseClass
+    {
+        public int MyFieldBase;
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public int MyFieldDerived;
+    }   
+    
+    public class Program
+    {
+        static void Main()
+        {
+            Type tbc = typeof(DerivedClass);                        // Get the type.
+            Console.WriteLine("Result is {0}.", tbc.Name);
+
+            Console.WriteLine("It has the following fields:");      // Use the type.
+            var fi = tbc.GetFields();
+            foreach (var f in fi)
+                Console.WriteLine(" {0}", f.Name);
+
+            Console.ReadLine();
+        }
+
+     }
+
+}
+
